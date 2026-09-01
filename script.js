c600bd0a84e05ef7bb1d010a689e07733ca038db
PierreGeisse <tvwv8w48zf@privaterelay.appleid.com>
2026-09-01 09:53:52 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-const io = new IntersectionObserver(entries => {
-  entries.forEach(e => { if(e.isIntersecting) e.target.classList.add('visible'); });
-},{threshold:.12});
-document.querySelectorAll('.reveal, section > *, article').forEach(el => {
-  if(!el.classList.contains('hero-image')) { el.classList.add('reveal'); io.observe(el); }
-});
